fabb1a905cf5c2cc67fd21c71be0d5495d83000b
Kamil Jankowski <kamil_jankowski@epam.com>
2022-07-15 21:55:40 +0200

    Readme updated and extension changed.

--- CollectionsTasks/src/io/github/kamil_jankowski/Task1/Main.java
+++ CollectionsTasks/src/io/github/kamil_jankowski/Task1/Main.java
@@ -3,5 +3,7 @@ package io.github.kamil_jankowski.Task1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Task 1");
+
+
     }
 }
--- README.md
+++
@@ -1,2 +0,0 @@
-# Coding-tasks
-This repository contains coding tasks and excercises for job interviews.
