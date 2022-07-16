b5a5b906af6eff4f21ca6fef43b6866157f47f0f
Kamil Jankowski <kamil_jankowski@epam.com>
2022-07-16 19:03:06 +0200

    Task 4: Initial commit

---
+++ CollectionsTasks/src/io/github/kamil_jankowski/task4/ArbitraryPointer.java
@@ -0,0 +1,9 @@
+package io.github.kamil_jankowski.task4;
+
+public class ArbitraryPointer {
+    public static void main(String[] args) {
+        System.out.println("Task 4: Copy Linked List with Arbitrary Pointer");
+
+
+    }
+}
