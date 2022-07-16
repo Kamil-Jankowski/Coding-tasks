b9178af305f32a87a2b946c031db3b4d70a3c017
Kamil Jankowski <kamil_jankowski@epam.com>
2022-07-16 17:49:20 +0200

    Task 1: List method added.

--- CollectionsTasks/src/io/github/kamil_jankowski/Task1/MissingNumber.java
+++ CollectionsTasks/src/io/github/kamil_jankowski/Task1/MissingNumber.java
@@ -1,9 +1,27 @@
 package io.github.kamil_jankowski.Task1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MissingNumber {
     public static void main(String[] args) {
         System.out.println("Task 1");
 
+        int[] positiveNumbers = initialize();
+
+        List<Integer> positiveNumbers2 = new ArrayList<>(5);
+        positiveNumbers2.add(4);
+        positiveNumbers2.add(1);
+        positiveNumbers2.add(5);
+        positiveNumbers2.add(3);
+
+        int missingNumber = findMissingNumber(positiveNumbers);
+        int missingNumber2 = findMissingNumber(positiveNumbers2);
+
+        System.out.printf("Missing numbers are:\n - missing number 1: %s\n - missing number 2: %s", missingNumber, missingNumber2);
+    }
+
+    private static int[] initialize() {
         int[] positiveNumbers = new int[8];
         positiveNumbers[0] = 3;
         positiveNumbers[1] = 7;
@@ -12,17 +30,7 @@ public class MissingNumber {
         positiveNumbers[4] = 8;
         positiveNumbers[5] = 4;
         positiveNumbers[6] = 5;
-
-        int[] positiveNumbers2 = new int[5];
-        positiveNumbers2[0] = 4;
-        positiveNumbers2[1] = 1;
-        positiveNumbers2[2] = 5;
-        positiveNumbers2[3] = 3;
-
-        int missingNumber = findMissingNumber(positiveNumbers);
-        int missingNumber2 = findMissingNumber(positiveNumbers2);
-
-        System.out.printf("Missing numbers are:\n - missing number 1: %s\n - missing number 2: %s", missingNumber, missingNumber2);
+        return positiveNumbers;
     }
 
     /**
@@ -43,6 +51,27 @@ public class MissingNumber {
             sumOfAllNumbersFrom1toN = sumOfAPair * amountOfPairs + middleValue;
         }
         int sumOfNumbers = 0;
+
+        for (int number : numbers) {
+            sumOfNumbers += number;
+        }
+
+        return sumOfAllNumbersFrom1toN - sumOfNumbers;
+    }
+
+    private static int findMissingNumber(List<Integer> numbers) {
+        int sumOfAllNumbersFrom1toN;
+        final int expectedSizeOfCompletelyFilledList = numbers.size() + 1;
+        final int amountOfPairs = expectedSizeOfCompletelyFilledList / 2;
+        final int sumOfAPair = expectedSizeOfCompletelyFilledList + 1;
+        final int middleValue = numbers.get(amountOfPairs + 1);
+        int sumOfNumbers = 0;
+
+        if(expectedSizeOfCompletelyFilledList % 2 == 0){
+            sumOfAllNumbersFrom1toN = sumOfAPair * amountOfPairs;
+        } else {
+            sumOfAllNumbersFrom1toN = sumOfAPair * amountOfPairs + middleValue;
+        }
 
         for (int number : numbers) {
             sumOfNumbers += number;
