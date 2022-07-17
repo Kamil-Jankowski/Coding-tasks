f86b0a90c6e72002409b51e746e185bd237e8611
Kamil Jankowski <kamil_jankowski@epam.com>
2022-07-17 18:56:25 +0200

    Task 4: Solved

--- CollectionsTasks/src/io/github/kamil_jankowski/task4/ListDuplicator.java
+++ CollectionsTasks/src/io/github/kamil_jankowski/task4/ListDuplicator.java
@@ -1,11 +1,32 @@
 package io.github.kamil_jankowski.task4;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class ListDuplicator {
 
     static ArbitraryLinkedList copy(ArbitraryLinkedList listToBeCopied) {
+        Node originalCurrentNode = listToBeCopied.head;
+        Node copiedCurrentNode;
 
+        Map<Node, Node> map = new HashMap<>();
 
+        while(originalCurrentNode != null){
+            copiedCurrentNode = new Node(originalCurrentNode.data);
+            map.put(originalCurrentNode, copiedCurrentNode);
+            originalCurrentNode = originalCurrentNode.next;
+        }
 
-        return null;
+        originalCurrentNode = listToBeCopied.head;
+
+        while(originalCurrentNode != null){
+            copiedCurrentNode = map.get(originalCurrentNode);
+            copiedCurrentNode.next = originalCurrentNode.next;
+            copiedCurrentNode.arbitrary = originalCurrentNode.arbitrary;
+
+            originalCurrentNode = originalCurrentNode.next;
+        }
+
+        return new ArbitraryLinkedList(map.get(listToBeCopied.head));
     }
 }
