064e78d2d52cea4033f0ed8c5375c9269e40b231
Kamil Jankowski <kamil_jankowski@epam.com>
2022-07-18 22:43:13 +0200

    Task 6: Initial commit.

---
+++ CollectionsTasks/src/io/github/kamil_jankowski/task6/BinarySearchTreeAnalyzer.java
@@ -0,0 +1,8 @@
+package io.github.kamil_jankowski.task6;
+
+class BinarySearchTreeAnalyzer {
+    public static void main(String[] args) {
+        System.out.println("Task 6: Determine if a binary tree is a binary search tree");
+
+    }
+}
