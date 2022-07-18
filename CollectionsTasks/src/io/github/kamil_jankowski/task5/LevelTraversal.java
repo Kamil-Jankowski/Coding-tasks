8e42181617806377ce4dcf256f26a54ecd6daaf1
Kamil Jankowski <kamil_jankowski@epam.com>
2022-07-18 21:31:18 +0200

    Task 5: Initial commit.

---
+++ CollectionsTasks/src/io/github/kamil_jankowski/task5/LevelTraversal.java
@@ -0,0 +1,9 @@
+package io.github.kamil_jankowski.task5;
+
+public class LevelTraversal {
+    public static void main(String[] args) {
+        System.out.println("Task 5: Level Order Traversal of Binary Tree\n");
+
+
+    }
+}
