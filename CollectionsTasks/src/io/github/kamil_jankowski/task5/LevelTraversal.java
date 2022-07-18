63f132b1fa85a14e24f4c1a63af199fd67f1795d
Kamil Jankowski <kamil_jankowski@epam.com>
2022-07-18 22:29:25 +0200

    Task 5: Solution corrected to reflect desired output formatting.

--- CollectionsTasks/src/io/github/kamil_jankowski/task5/LevelTraversal.java
+++ CollectionsTasks/src/io/github/kamil_jankowski/task5/LevelTraversal.java
@@ -1,14 +1,15 @@
 package io.github.kamil_jankowski.task5;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class LevelTraversal {
     public static void main(String[] args) {
         System.out.println("Task 5: Level Order Traversal of Binary Tree\n");
 
-        /* creating a binary tree and entering
-         the nodes */
+        // creating a binary tree and entering the nodes
         BinaryTree tree = new BinaryTree();
         tree.root = new Node(100);
         tree.root.left = new Node(50);
@@ -17,21 +18,43 @@ public class LevelTraversal {
         tree.root.left.right = new Node(75);
         tree.root.right.right = new Node(350);
 
+        String traversal = traverseBinaryTreeLevelOrder(tree);
+
         System.out.println("Level order traversal of binary tree is - ");
-        printLevelOrder(tree);
+        System.out.println(traversal);
     }
 
-    private static void printLevelOrder(BinaryTree tree) {
-        Queue<Node> nodes = new LinkedList<>();
-        nodes.add(tree.root);
+    private static String traverseBinaryTreeLevelOrder(BinaryTree tree) {
+        StringBuilder traversal = new StringBuilder();
 
-        while (!nodes.isEmpty()){
-            final Node current = nodes.peek();
+        List<Queue<Node>> queues = new ArrayList<>();
+        Queue<Node> currentLevelNodes = new LinkedList<>();
+        Queue<Node> nextLevelNodes = new LinkedList<>();
 
-            if (current.left != null) nodes.add(current.left);
-            if (current.right != null) nodes.add(current.right);
+        queues.add(0, currentLevelNodes);
+        queues.add(1, nextLevelNodes);
 
-            System.out.println(nodes.poll().data);
+        currentLevelNodes.add(tree.root);
+        int level = 0;
+
+        while (!currentLevelNodes.isEmpty()){
+            final Node current = currentLevelNodes.poll();
+            traversal.append(current.data);
+
+            if (current.left != null) nextLevelNodes.add(current.left);
+            if (current.right != null) nextLevelNodes.add(current.right);
+
+            // formatting the output
+            if (currentLevelNodes.isEmpty()){
+                traversal.append("\n");
+                level++;
+            // Making nextLevelNodes queue the CURRENT one, empty currentLevelNodes queue to be used as a new NEXT one
+                currentLevelNodes = queues.get(level % 2);
+                nextLevelNodes = queues.get((level + 1) % 2);
+            } else {
+                traversal.append(", ");
+            }
         }
+        return traversal.toString();
     }
 }
