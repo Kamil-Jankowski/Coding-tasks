c5efffe147ba519c6d210d4137f49fa812659626
Kamil Jankowski <kamil_jankowski@epam.com>
2022-07-20 11:16:35 +0200

    Task 7: Initial commit.

---
+++ CollectionsTasks/src/io/github/kamil_jankowski/task7/Main.java
@@ -0,0 +1,9 @@
+package io.github.kamil_jankowski.task7;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Task 7: String segmentation\n");
+
+
+    }
+}
