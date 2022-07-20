b71a2356e26945e37dcdb90a2193e00af7624e33
Kamil Jankowski <kamil_jankowski@epam.com>
2022-07-20 17:49:53 +0200

    Task 8: Solved using StringBuilder.

--- CollectionsTasks/src/io/github/kamil_jankowski/task8/SentenceReversal.java
+++ CollectionsTasks/src/io/github/kamil_jankowski/task8/SentenceReversal.java
@@ -1,8 +1,24 @@
 package io.github.kamil_jankowski.task8;
 
 public class SentenceReversal {
+
+    public static final String EMPTY_SPACE = " ";
+
     public static void main(String[] args) {
         System.out.println("Task 8: Reverse words in a sentence");
 
+        System.out.println(reverseSentence("Hello World!"));
+        System.out.println(reverseSentence("We love Java"));
+        System.out.println(reverseSentence("To be, or not to be?"));
+    }
+
+    private static String reverseSentence(String sentence){
+        StringBuilder sb = new StringBuilder();
+        final String[] splitString = sentence.split("\\W+");
+        final int length = splitString.length;
+        for (int i = length-1; i >= 0; i--){
+            sb.append(splitString[i]).append(EMPTY_SPACE);
+        }
+        return sb.toString();
     }
 }
