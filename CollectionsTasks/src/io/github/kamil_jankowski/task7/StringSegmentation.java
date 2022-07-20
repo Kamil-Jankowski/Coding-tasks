794699aba8b0d56077440c36d05635ba1bb45f45
Kamil Jankowski <kamil_jankowski@epam.com>
2022-07-20 14:56:20 +0200

    Task 7: Solved in O(2^n) runtime complexity.

--- CollectionsTasks/src/io/github/kamil_jankowski/task7/StringSegmentation.java
+++ CollectionsTasks/src/io/github/kamil_jankowski/task7/StringSegmentation.java
@@ -1,6 +1,7 @@
 package io.github.kamil_jankowski.task7;
 
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Set;
 
 public class StringSegmentation {
@@ -9,7 +10,7 @@ public class StringSegmentation {
 
         Set<String> dictionary = new HashSet<>();
         String segmantable = "hellonow";
-        String nonSegmantable = "HellowThere";
+        String nonSegmantable = "HelloThere";
 
         dictionary.add("hello");
         dictionary.add("hell");
@@ -30,6 +31,18 @@ public class StringSegmentation {
     }
 
     private static boolean canSegmentString(String stringToVerify, Set<String> dictionary) {
-        return false;
+        boolean isSegmentable = false;
+        for (int i = 0; i < stringToVerify.length(); i++) {
+            String firstString = stringToVerify.substring(0, i+1);
+            String secondString = stringToVerify.substring(i+1);
+            if (dictionary.contains(firstString.toLowerCase())) {
+                if (dictionary.contains(secondString.toLowerCase()) || secondString.length() == 0) {
+                    return true;
+                } else {
+                    isSegmentable = canSegmentString(secondString, dictionary);
+                }
+            }
+        }
+        return isSegmentable;
     }
 }
