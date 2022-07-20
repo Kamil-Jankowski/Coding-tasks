3229a7ce5c000e6a8b05b244537237d6161231ef
Kamil Jankowski <kamil_jankowski@epam.com>
2022-07-20 18:58:36 +0200

    Task 9: Initial commit.

---
+++ CollectionsTasks/src/io/github/kamil_jankowski/task9/CoinChangingProblem.java
@@ -0,0 +1,8 @@
+package io.github.kamil_jankowski.task9;
+
+public class CoinChangingProblem {
+    public static void main(String[] args) {
+        System.out.println("Task 9: Coin Changing Problem");
+
+    }
+}
