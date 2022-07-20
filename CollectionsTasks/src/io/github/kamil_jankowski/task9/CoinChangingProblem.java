a09afdcac2c2b4ec29a8dc8ac09324566fe975ae
Kamil Jankowski <kamil_jankowski@epam.com>
2022-07-20 21:49:49 +0200

    Task 9: Solved. Comments added.

--- CollectionsTasks/src/io/github/kamil_jankowski/task9/CoinChangingProblem.java
+++ CollectionsTasks/src/io/github/kamil_jankowski/task9/CoinChangingProblem.java
@@ -13,16 +13,37 @@ public class CoinChangingProblem {
         System.out.print(result);
     }
 
+    /* Dynamic Programming Java implementation of Coin Change problem */
     private static int solveCoinChange(int[] denominations, int amount) {
+        /*
+         Time complexity of this function: O(mn)
+         Space Complexity of this function: O(n)
+
+         solution[i] will be storing the number of solutions
+         for amount i. We need n+1 rows as the table is
+         constructed in bottom up manner using the base
+         case (n = 0)
+        */
         int[] solution = new int[amount + 1];
+
+        /*
+         Base case (If given value i is 0)
+        */
         solution[0] = 1;
 
+        /*
+         Check how many possibilities each denomination gives to gather required amount.
+         Passes through each denomination one by one, adding to previously calculated possibilities from other denominations,
+         and the current one.
+        */
         for (int den: denominations){
+            System.out.println("Denomination: " + den);
             for (int i = den; i < solution.length; i++) {
                 solution[i] += solution[i - den];
+                System.out.println("Index " + i + " = " + solution[i]);
             }
         }
-
+        System.out.println(Arrays.toString(solution));
         return solution[solution.length-1];
     }
 }
