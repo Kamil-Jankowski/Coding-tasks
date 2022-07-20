6eb0daee5c4d6bdfb7c9a641f294a5c45950956d
Kamil Jankowski <kamil_jankowski@epam.com>
2022-07-20 17:20:20 +0200

    Task 8: Initial commit.

---
+++ CollectionsTasks/src/io/github/kamil_jankowski/task8/SentenceReversal.java
@@ -0,0 +1,8 @@
+package io.github.kamil_jankowski.task8;
+
+public class SentenceReversal {
+    public static void main(String[] args) {
+        System.out.println("Task 8: Reverse words in a sentence");
+
+    }
+}
