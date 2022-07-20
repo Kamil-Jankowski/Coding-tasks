8a27971a0613054a0f5ca98c4d04e4b16e0cc8bc
Kamil Jankowski <kamil_jankowski@epam.com>
2022-07-20 19:00:03 +0200

    Task 9: Testing logic added in main method.

--- CollectionsTasks/src/io/github/kamil_jankowski/task9/CoinChangingProblem.java
+++ CollectionsTasks/src/io/github/kamil_jankowski/task9/CoinChangingProblem.java
@@ -1,8 +1,19 @@
 package io.github.kamil_jankowski.task9;
 
+import java.util.Arrays;
+
 public class CoinChangingProblem {
     public static void main(String[] args) {
         System.out.println("Task 9: Coin Changing Problem");
 
+        int[] denominations = new int[] {1, 3, 5};
+        int amount = 7;
+        int result = solveCoinChange(denominations, amount);
+        System.out.print("SolveCoinChange(" + Arrays.toString(denominations) +  ", " + amount + ") = ");
+        System.out.print(result);
+    }
+
+    private static int solveCoinChange(int[] denominations, int amount) {
+        return 0;
     }
 }
