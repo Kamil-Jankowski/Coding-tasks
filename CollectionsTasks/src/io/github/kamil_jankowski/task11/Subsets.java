b9448d6e8b9c3cfbda1bdcb40a624cf7002cc852
Kamil Jankowski <kamil_jankowski@epam.com>
2022-07-21 10:08:20 +0200

    Task 11: Initial commit.

---
+++ CollectionsTasks/src/io/github/kamil_jankowski/task11/Subsets.java
@@ -0,0 +1,9 @@
+package io.github.kamil_jankowski.task11;
+
+public class Subsets {
+    public static void main(String[] args) {
+        System.out.println("Task 11: Find all subsets of a given set of integers");
+
+
+    }
+}
