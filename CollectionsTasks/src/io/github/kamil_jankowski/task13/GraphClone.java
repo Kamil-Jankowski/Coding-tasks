1963ca36dfce9b47d19067f655a4a5b8a7b823d6
Kamil Jankowski <kamil_jankowski@epam.com>
2022-07-21 14:32:25 +0200

    Task 13: Initial commit.

---
+++ CollectionsTasks/src/io/github/kamil_jankowski/task13/GraphClone.java
@@ -0,0 +1,9 @@
+package io.github.kamil_jankowski.task13;
+
+public class GraphClone {
+    public static void main(String[] args) {
+        System.out.println("Task 13: Clone a graph");
+
+
+    }
+}
