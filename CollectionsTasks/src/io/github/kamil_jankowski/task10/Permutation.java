f28fff04e9b18c82eba0757ed28261b9f300a73e
Kamil Jankowski <kamil_jankowski@epam.com>
2022-07-21 08:51:03 +0200

    Task 10: Initial commit.

---
+++ CollectionsTasks/src/io/github/kamil_jankowski/task10/Permutation.java
@@ -0,0 +1,8 @@
+package io.github.kamil_jankowski.task10;
+
+public class Permutation {
+    public static void main(String[] args) {
+        System.out.println("Task 10: Find Kth Permutation");
+
+    }
+}
