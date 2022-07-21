a20b7e3a2dbf881d61cf4a69bb6406f6494b92e1
Kamil Jankowski <kamil_jankowski@epam.com>
2022-07-21 13:36:01 +0200

    Task 12: Initial commit.

---
+++ CollectionsTasks/src/io/github/kamil_jankowski/task12/BraceCombinations.java
@@ -0,0 +1,9 @@
+package io.github.kamil_jankowski.task12;
+
+public class BraceCombinations {
+    public static void main(String[] args) {
+        System.out.println("Task 12: Print balanced brace combinations");
+
+
+    }
+}
