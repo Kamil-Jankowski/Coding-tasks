8170e1f02d04287bc8c44b62529bb5c7cbc9b06c
Kamil Jankowski <kamil_jankowski@epam.com>
2022-07-30 18:27:17 +0200

    Task 14: Initial commit.

---
+++ CollectionsTasks/src/io/github/kamil_jankowski/task14/LowHigh.java
@@ -0,0 +1,9 @@
+package io.github.kamil_jankowski.task14;
+
+public class LowHigh {
+    public static void main(String[] args) {
+        System.out.println("Task 14: Find Low/High Index");
+
+
+    }
+}
