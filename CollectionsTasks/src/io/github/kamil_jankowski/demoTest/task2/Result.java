902fb29c7ffbc3fe93e99fc5d235e3cc38c34654
Kamil Jankowski <kamil_jankowski@epam.com>
2022-08-17 13:10:16 +0200

    Task 2: corrected.

--- CollectionsTasks/src/io/github/kamil_jankowski/demoTest/task2/Result.java
+++ CollectionsTasks/src/io/github/kamil_jankowski/demoTest/task2/Result.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Result {
+    public static final String BORDER = "|";
 
     /*
      * Complete the 'numberOfItems' function below.
@@ -26,38 +27,57 @@ class Result {
 
     public static List<Integer> numberOfItems(String s, List<Integer> startIndices, List<Integer> endIndices) {
         List<Integer> numberOfItems = new ArrayList<>();
-        int minimalSize = Math.min(startIndices.size(), endIndices.size());
-        StringBuilder sb = new StringBuilder(s);
 
-        for (int i = 0; i < minimalSize; i++){
-            // Create substring for a given pair of indices
-            final String substring = sb.substring(startIndices.get(i), endIndices.get(i));
-            // Count the amount of items in closed compartments
-            int counter = 0;
+        if (s == null) return numberOfItems;
 
-            int start = 0;
-            for(int j = 0; j < substring.length(); j++){
-                /*If | is noticed, count items.*/
-                if(substring.charAt(j) == '|'){
-                    counter += countItems(substring, start, j-1);
-                    start = j+1;  // Defines new starting point
-                }
+        arrayEqualizer(s, startIndices, endIndices);
+
+        for (int i = 0; i < startIndices.size(); i++) {
+            final int items;
+            if (startIndices.get(i) < 0 || endIndices.get(i) > s.length() - 1){
+                System.out.println("Given borders are out of bounds");
+                items = 0;
+            } else {
+                items = countItems(s, startIndices, endIndices, i);
             }
-            numberOfItems.add(counter);
+            numberOfItems.add(items);
         }
-        System.out.println(numberOfItems);
+        System.out.println("Number of items found for given indices pairs: " + numberOfItems);
         return numberOfItems;
     }
 
-    private static int countItems(String string, int start, int end) {
-        int counter = 0;
-        System.out.println("Counting items in: " + string);
-        System.out.println("Specifically in: " + string.substring(start, end+1));
-        for (int i = start; i <= end; i++){
-            if (string.charAt(i) == '*') counter++;
-            System.out.println(string.charAt(i));
+    private static void arrayEqualizer(String s, List<Integer> startIndices, List<Integer> endIndices) {
+        int startIndex = 0;
+        int endIndex = s.length()-1;
+        final int startSize = startIndices.size();
+        final int endSize = endIndices.size();
+
+        if (startSize > endSize){
+            for (int i = 0; i < startSize - endSize; i++){
+                endIndices.add(endIndex);
+            }
         }
-        return counter;
+
+        if (startSize < endSize){
+            for (int i = 0; i < endSize - startSize; i++){
+                startIndices.add(startIndex);
+            }
+        }
+    }
+
+    private static int countItems(String s, List<Integer> startIndices, List<Integer> endIndices, int i) {
+        String range = s.substring(startIndices.get(i), endIndices.get(i)+1);
+
+        int openingIndex = range.indexOf(BORDER);
+        int closingIndex = range.lastIndexOf(BORDER);
+        String closedRange = range.substring(openingIndex, closingIndex);
+        final String items = closedRange.replaceAll("\\|", "");
+
+        System.out.println("Range to be examined: " + range);
+        System.out.println("Closed range of items: " + closedRange);
+        System.out.println("Just enclosed items: " + items);
+
+        return items.length();
     }
 
 }
